bb4f7eacd8a1f1faf63a852a364cfca562f708a8
Sinan Şenkul <101463814+SinanSenkul@users.noreply.github.com>
2022-05-26 18:47:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const getPage = async () => {
     const config = { headers: { Accept: 'application/json' } }
     const res = await axios.get('https://api.tvmaze.com/search/shows?q=' + entry.value, config);
     let div = document.createElement('div');
-    //div.classList.add('div');
-    //console.log(res.data[0].show.url);
     if (res.data.length === 0) {
         let hdr = document.createElement('h1');
         hdr.innerText = 'No Match Was Found';
@@ -12,25 +10,28 @@ const getPage = async () => {
         div.appendChild(hdr);
         divCount = 1;
     }
-    for (let i = 0; i < res.data.length; i++) {
-        let a = document.createElement('a');
-        a.href = res.data[i].show.url;
-        a.target = "_blank";
-        let img = document.createElement('img');
-        img.classList.add('img');
-        if (res.data[i].show.image) {
-            img.src = res.data[i].show.image.medium;
-            a.appendChild(img);
-            div.appendChild(a);
-        }
-    }
-    if (divCount === 0) {
-        document.body.appendChild(div);
-        divCount = 1;
-    }
     else {
-        document.body.removeChild(document.body.lastChild);
-        document.body.appendChild(div);
+        for (let i = 0; i < res.data.length; i++) {
+            let a = document.createElement('a');
+            a.href = res.data[i].show.url;
+            a.target = "_blank";
+            let img = document.createElement('img');
+            img.classList.add('img');
+            if (res.data[i].show.image) {
+                img.src = res.data[i].show.image.medium;
+                a.appendChild(img);
+                div.appendChild(a);
+            }
+        }
+        if (divCount === 0) {
+            document.body.appendChild(div);
+            divCount = 1;
+        }
+        else {
+            document.body.removeChild(document.body.lastChild);
+            document.body.appendChild(div);
+            divCount = 1;
+        }
     }
 }
 
@@ -38,5 +39,3 @@ var entry = document.querySelector('#entry');
 var submit = document.querySelector('#submit');
 submit.addEventListener('click', getPage);
 var divCount = 0;
-
-
